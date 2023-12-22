f1cd0c9cd3cca1a9a9e7eb5ceaba7c92034df599
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-22 13:23:04 +0200

    Added express config func.

---
+++ backend/src/configs/expressConfig.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const router = require("../router");
+const setupExpress = (app) => {
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    // Auth middleware here.
+    app.use(router);
+};
+
+module.exports = setupExpress;
