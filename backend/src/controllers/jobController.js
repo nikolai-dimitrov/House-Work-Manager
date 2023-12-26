de18e363c5def9a42360f1db66836c9729cab58e
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-26 16:59:24 +0200

    Added method that remove file from cloudinary

--- backend/src/controllers/jobController.js
+++ backend/src/controllers/jobController.js
@@ -57,6 +57,7 @@ router.delete("/:id", async (req, res) => {
         if (!job) {
             return res.status(404).json({ error: "No such job" });
         }
+        await cloudinary.uploader.destroy(job.image.public_id);
         res.status(200).json(job);
     } catch (error) {
         res.status(400).json({ error: error.message });
