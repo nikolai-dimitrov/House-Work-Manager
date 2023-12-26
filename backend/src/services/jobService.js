e1b8ee8e05b78434d45b9d32e4c781ac27015b76
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-26 15:51:35 +0200

    Added job service methods.

---
+++ backend/src/services/jobService.js
@@ -0,0 +1,21 @@
+const Job = require("../models/Job");
+
+exports.getAll = () => {
+    return Job.find({});
+};
+
+exports.getOne = (id) => {
+    return Job.findById(id);
+};
+
+exports.create = (data) => {
+    return Job.create(data);
+};
+
+exports.update = (id, newData) => {
+    return Job.findByIdAndUpdate(id, newData, { runValidators: true });
+};
+
+exports.remove = (id) => {
+    return Job.findByIdAndDelete(id);
+};
