b736268eac47b6cf08a5985284b140f52aa7f935
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-28 13:43:45 +0200

    Added handlers login and register path

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,37 @@
 const router = require("express").Router();
-router.get("/register", (req, res) => {
-    res.json({ msg: "register path" });
+const userService = require("../services/userService");
+
+// Login
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let { user, token } = await userService.login({ email, password });
+        user = { token, _id: user._id, email: user.email };
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
+
+// Register
+router.post("/register", async (req, res) => {
+    const { email, password, repeatPassword } = req.body;
+    try {
+        let { newUser, token } = await userService.register({
+            email,
+            password,
+            repeatPassword,
+        });
+        const user = { token, _id: newUser._id, email: newUser.email };
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+    res.json({ msg: "logout" });
+});
+
 module.exports = router;
