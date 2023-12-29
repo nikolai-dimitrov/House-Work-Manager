e00b060c784caf56837e41a857bcb31a3a0add0a
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-29 19:30:52 +0200

    Added auth middleware.

--- backend/src/configs/expressConfig.js
+++ backend/src/configs/expressConfig.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const router = require("../router");
+const { authentication } = require("../middlewares/authMiddleware");
 const setupExpress = (app) => {
     // app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
     app.use(cookieParser());
-    // Auth middleware here.
+    // Auth middleware
+    app.use(authentication);
     app.use(router);
 };
 
---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require("../libs/jwt");
+const { JWT_SECRET } = require("../constants");
+exports.authentication = async (req, res, next) => {
+    const { authorization } = req.headers;
+    if (authorization) {
+        const token = authorization.split(" ")[1];
+        try {
+            const userId = await jwt.verify(token, JWT_SECRET);
+            req.userId = userId;
+        } catch (error) {
+            res.status(401).json({ error: "Invalid access token." });
+        }
+    } else {
+        next();
+    }
+};
+
+exports.authRequired = (isRequired) => {
+    return function (req, res, next) {
+        if (isRequired === true) {
+            if (!req.userId) {
+                res.status(401).json({ error: "Login required." });
+                return;
+            }
+            next();
+        } else {
+            if (req.userId) {
+                res.status(403).json({ error: "You are already logged in." });
+                return;
+            }
+            next();
+        }
+    };
+};
