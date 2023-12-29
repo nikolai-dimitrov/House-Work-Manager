8e380cea5e54b0855b34ea26bd40d194b29296f9
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-29 19:32:32 +0200

    Example file upload functionality (test)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+    const [img, setImg] = useState("");
+    const sendFile = async (e) => {
+        e.preventDefault();
+        let file = e.target.parentElement.querySelector("input").files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            setImg(reader.result);
+        };
+    };
+    console.log(img);
+    return (
+        <>
+            <form action="">
+                <label htmlFor="">Image File</label>
+                <input type="file" />
+                <button onClick={sendFile}>Send</button>
+            </form>
+        </>
+    );
 }
 
-export default App
+export default App;
