4564fe3ce437d61e823916249471c9066ffc78ff
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-31 16:04:04 +0200

    Added user relation and test default profile picture.

--- backend/src/models/Profile.js
+++ backend/src/models/Profile.js
@@ -4,38 +4,35 @@ const profileSchema = new mongoose.Schema({
         type: "string",
         required: [true, "First Name is required."],
         minLength: [3, "First Name must be at least 3 characters!"],
-        default: "",
     },
 
     lastName: {
         type: "string",
         required: [true, "Last Name is required."],
         minLength: [3, "Last Name must be at least 4 characters!"],
-        default: "",
     },
 
     phoneNumber: {
         type: "string",
         required: [true, "Phone Number is required."],
         match: [/^0[1-9]{1}[0-9]{8}$/, "Phone Number is not valid!"],
-        default: "",
     },
-
-    //Add default profile picture
+    // TODO: Folder with profile pictures in cloudinary and change houseImages/ -> profileImages
     profileImg: {
         public_id: {
             type: "String",
-            required: true,
+            default: "houseImages/ui4jtdbcgjyhyv3ybdbg",
         },
         url: {
             type: "String",
-            required: true,
+            default:
+                "https://res.cloudinary.com/drbrcolnz/image/upload/v1704027745/houseImages/ui4jtdbcgjyhyv3ybdbg.png",
         },
     },
 
-    isActivated: {
-        type: "boolean",
-        default: false,
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
     },
 });
 const Profile = mongoose.model("Profile", profileSchema);
