7c3ed4a31b1fad1fd19ccd9b0439671c5a6da836
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-31 16:03:29 +0200

    Added new route for profile

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userController = require("./controllers/userController");
 const jobController = require("./controllers/jobController");
+const profileController = require("./controllers/profileController");
 router.use("/api/users", userController);
 router.use("/api/jobs", jobController);
+router.use("/api/profile", profileController);
 
 module.exports = router;
