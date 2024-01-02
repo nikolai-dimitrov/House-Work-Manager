0451b83304ff6af01fdf21949e5d62a4e0b3e8fb
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-01-02 19:53:24 +0200

    Added profileRequired middleware for specific routes.

--- backend/src/controllers/jobController.js
+++ backend/src/controllers/jobController.js
@@ -4,8 +4,10 @@ const mongoose = require("mongoose");
 const cloudinary = require("../configs/cloudinaryConfig");
 const { sanitizeData } = require("../middlewares/sanitizer");
 const { authRequired } = require("../middlewares/authMiddleware");
+const { profileRequired } = require("../middlewares/profileMiddleware");
 
 // Retrieve all Jobs
+
 router.get("/", async (req, res) => {
     try {
         const jobs = await jobService.getAll();
@@ -33,28 +35,34 @@ router.get("/:id", async (req, res) => {
 });
 
 //Create Job
-router.post("/", sanitizeData(), authRequired(true), async (req, res) => {
-    const data = { ...req.body, owner: req.user?._id };
+router.post(
+    "/",
+    sanitizeData(),
+    authRequired(true),
+    profileRequired,
+    async (req, res) => {
+        const data = { ...req.body, owner: req.user?._id };
 
-    try {
-        const result = await cloudinary.uploader.upload(data.image, {
-            folder: "houseImages",
-        });
+        try {
+            const result = await cloudinary.uploader.upload(data.image, {
+                folder: "houseImages",
+            });
 
-        data.image = {
-            public_id: result.public_id,
-            url: result.secure_url,
-        };
-        const job = await jobService.create(data);
+            data.image = {
+                public_id: result.public_id,
+                url: result.secure_url,
+            };
+            const job = await jobService.create(data);
 
-        res.status(201).json(job);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+            res.status(201).json(job);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
-});
+);
 
 //Delete Job
-router.delete("/:id", authRequired(true), async (req, res) => {
+router.delete("/:id", authRequired(true), profileRequired, async (req, res) => {
     try {
         const id = req.params.id;
         if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -72,35 +80,41 @@ router.delete("/:id", authRequired(true), async (req, res) => {
 });
 
 // Edit Job
-router.put("/:id", sanitizeData(), authRequired(true), async (req, res) => {
-    try {
-        const id = req.params.id;
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(404).json({ error: "No such job" });
+router.put(
+    "/:id",
+    sanitizeData(),
+    authRequired(true),
+    profileRequired,
+    async (req, res) => {
+        try {
+            const id = req.params.id;
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                return res.status(404).json({ error: "No such job" });
+            }
+
+            const data = { ...req.body };
+            const currentJob = await jobService.getOne(id);
+
+            if (!currentJob) {
+                return res.status(404).json({ error: "No such job" });
+            }
+
+            await cloudinary.uploader.destroy(currentJob.image.public_id);
+            const newImage = await cloudinary.uploader.upload(data.image, {
+                folder: "houseImages",
+            });
+
+            data.image = {
+                public_id: newImage.public_id,
+                url: newImage.secure_url,
+            };
+
+            const job = await jobService.update(id, data);
+            res.status(200).json(job);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
         }
-
-        const data = { ...req.body };
-        const currentJob = await jobService.getOne(id);
-
-        if (!currentJob) {
-            return res.status(404).json({ error: "No such job" });
-        }
-
-        await cloudinary.uploader.destroy(currentJob.image.public_id);
-        const newImage = await cloudinary.uploader.upload(data.image, {
-            folder: "houseImages",
-        });
-
-        data.image = {
-            public_id: newImage.public_id,
-            url: newImage.secure_url,
-        };
-
-        const job = await jobService.update(id, data);
-        res.status(200).json(job);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
     }
-});
+);
 
 module.exports = router;
