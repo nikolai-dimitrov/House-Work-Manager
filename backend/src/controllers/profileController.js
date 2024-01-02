afc1b2b68b2990f2a2619e3504aa3d28b1ea229d
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-01-02 18:05:13 +0200

    Added cloudinary uploader for profile images

--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { authRequired } = require("../middlewares/authMiddleware");
 const profileService = require("../services/profileService");
+const cloudinary = require("../configs/cloudinaryConfig");
+
 // Get user's profile details
 router.get("/details", authRequired(true), async (req, res) => {
     try {
@@ -16,6 +18,20 @@ router.get("/details", authRequired(true), async (req, res) => {
 router.post("/create", authRequired(true), async (req, res) => {
     try {
         const profileData = { ...req.body, owner: req.user?._id };
+        if (profileData.profileImg) {
+            const result = await cloudinary.uploader.upload(
+                profileData.profileImg,
+                {
+                    folder: "profileImages",
+                }
+            );
+
+            profileData.profileImg = {
+                public_id: result.public_id,
+                url: result.secure_url,
+            };
+        }
+
         const profile = await profileService.create(profileData);
         res.status(201).json({ profile });
     } catch (error) {
@@ -28,6 +44,19 @@ router.put("/edit", authRequired(true), async (req, res) => {
     try {
         const userId = req.user?._id;
         const profileData = { ...req.body };
+
+        const newImage = await cloudinary.uploader.upload(
+            profileData.profileImg,
+            {
+                folder: "profileImages",
+            }
+        );
+
+        profileData.profileImg = {
+            public_id: newImage.public_id,
+            url: newImage.secure_url,
+        };
+
         const profile = await profileService.update(profileData, userId);
         res.status(201).json({ profile });
     } catch (error) {
@@ -35,6 +64,5 @@ router.put("/edit", authRequired(true), async (req, res) => {
     }
 });
 
-// 4. Create profile after user register and log in -> redirect to profile section
 // 5. If not profile created cannot post edit delete jobs only view (like unauthenticated user)
 module.exports = router;
