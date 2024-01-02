db1f1db451401321dbf7541ba12624e2428913e0
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-01-02 19:53:05 +0200

    Formatted file.

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userController = require("./controllers/userController");
 const jobController = require("./controllers/jobController");
 const profileController = require("./controllers/profileController");
+
 router.use("/api/users", userController);
 router.use("/api/jobs", jobController);
 router.use("/api/profile", profileController);
