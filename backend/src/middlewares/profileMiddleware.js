ec96b7d054723585c21f8f8ca0dfb901895251e2
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-01-05 17:53:23 +0200

    Added error handling in middleware

--- backend/src/middlewares/profileMiddleware.js
+++ backend/src/middlewares/profileMiddleware.js
@@ -1,10 +1,15 @@
 const profileService = require("../services/profileService");
 exports.profileRequired = async (req, res, next) => {
     const userId = req.user?._id;
-    const hasProfile = await profileService.profileExists(userId);
-    if (!hasProfile) {
-        res.status(403).json({ message: "Profile is required" });
-        return;
+    try {
+        const hasProfile = await profileService.profileExists(userId);
+        if (!hasProfile) {
+            res.status(403).json({ message: "Please create your profile." });
+            return;
+        }
+        next();
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+        return
     }
-    next();
 };
