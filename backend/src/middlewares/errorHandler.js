2ef01340a090737d7067a866bfd85db2979ed853
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-01-06 19:44:51 +0200

    Added additional properties

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,9 +1,11 @@
 const errorHandler = (err, req, res, next) => {
     const errHttpCode = err.httpCode || 400;
     const errMsg = err.message || "Something went wrong";
+    const isCustom = err.isCustom || false;
     res.status(errHttpCode).json({
         // success: false,
-        // status: errStatus,
+        status: errHttpCode,
+        isCustomErr: isCustom,
         message: errMsg,
         // stack: process.env.NODE_ENV === "development" ? err.stack : {},
         stack: err.stack,
